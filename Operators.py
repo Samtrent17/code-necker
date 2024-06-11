cce50d5f7417e1e5b70fac880f0c655f0ca0f83e
Samtrent17 <xamsamuel079@gmail.com>
2024-06-11 12:51:04 +0300

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,30 @@
+#Print 
+print(5+3)
+print(10%3)
+print(5**2)
+print(5//3)
+print(6//3)
+#assigning
+z=(2+3)
+x=(5+2)
+y=(5//3)
+z *=2
+print(z)
+x**=3
+print(x)
+
+joan = 18
+max = 20
+print(joan>max)
+print(joan==max)
+print(joan!=max)
+print((max>= 18) and (max>joan))
+print((max>=joan) or (max>joan))
+print((max>=joan) or (max>joan) and (max!=21) and ('max'!='james'))
+
+message = 'food'
+dict1 = {1: 'n' , 2: 'm' , 3: 'h' , 4: 'f'}
+#check if f is in the message
+print('f' in message)
+print('g' in message)
+
---
+++ bytes.py
@@ -0,0 +1,41 @@
+#Variable for age
+age = 19
+#Data type
+print(type(age))
+#Variable for Name
+name = 'Nangoli Samuel'
+#Data type
+print(type(name)) 
+#Variable for student status
+student_status = (True)
+#Data type
+print(type(student_status))
+#Variable for student balance
+student_balance = 1200000.48
+#Data type
+print(type(student_balance))
+#Using input
+age_dynamic = int(input('What is your age ? '))
+#Data type
+print('age',age_dynamic,'Type',type(age_dynamic))
+#name dyanmic
+name_dynamic = input('What is your name ?')
+#Data type
+print('name',name_dynamic,'Type',type(name_dynamic))
+#student status dynamic
+student_status_dynamic = bool(input('What is your student status ?'))
+#Data type
+print('student_status',student_status_dynamic,'type',type(student_status_dynamic))
+#student balance dynamic
+student_balance_dynamic = float(input('What is your student balance ?'))
+#Data type
+print('student_balance',student_balance_dynamic,'type',type(student_balance_dynamic))
+#Printing
+print(int(20.9))
+print(int(30.7))
+print(int('21'))
+print(float(20))
+
+
+
+
